75a9fb9c276bbf652ab014787df38496007b2f78
shahnirav <shah.nirav@me.com>
2017-02-23 08:20:34 +0000

    refactor

--- main/routes/home.js
+++ main/routes/home.js
@@ -1,5 +1,9 @@
-module.exports = function (app, passport) {
+module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index');
+    });
+
+    app.get('/order', function (req, res) {
+        res.render('order');
     });
 };
