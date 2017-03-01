94dc421540cea559f45d6bb106c8a4365bc895d2
shahnirav <shah.nirav@me.com>
2017-03-01 21:03:04 +0000

    commit

--- main/index.js
+++ main/index.js
@@ -39,6 +39,7 @@ var salt = bcrypt.genSaltSync(10);
 require('./routes/home')(app, passport);
 require('./routes/mailchimp')(app, config);
 require('./routes/zendesk')(app, config);
+require('./routes/payumoney')(app, config);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- main/routes/payumoney.js
+++ main/routes/payumoney.js
@@ -1,16 +1,16 @@
 var crypto = require('crypto'),
     text = 'hello bob',
-    key = 'mysecret key'
+    key = 'W2Yp27WURs';
 
 
 
 module.exports = function (app) {
     app.get('/hash', function (req, res) {
-        var hash = crypto.createHmac('sha512', key);
+        var hash = crypto.createHash('sha512', key);
         hash.update(text);
         var value = hash.digest('hex');
         console.log(value);
-
-        res.send({hash:value});
+        res.setHeader("Authorization","AKJyS9Oh");
+        res.redirect('https://secure.payu.in/_payment?amount=100');
     })
 }
