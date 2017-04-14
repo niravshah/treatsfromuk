3a593f0133c1895d091b117e49986eea4d0f8506
shahnirav <shah.nirav@me.com>
2017-04-14 18:53:54 +0100

    commit

--- main/config.js
+++ main/config.js
@@ -2,13 +2,15 @@ var config = [];
 
 config['dev'] = {
     mongoUrl: 'mongodb://localhost/treatsfromuk',
-    mc_api_key:'512732dd48fe0b740b50bf9f5217449e-us15'
+    mc_api_key:'512732dd48fe0b740b50bf9f5217449e-us15',
+    mg_api_key:'key-f04b0e48c5a6fc439036aa284b5e15e6'
 
 };
 
 config['production'] = {
     mongoUrl: 'mongodb://localhost/treatsfromuk',
-    mc_api_key:'512732dd48fe0b740b50bf9f5217449e-us15'
+    mc_api_key:'512732dd48fe0b740b50bf9f5217449e-us15',
+    mg_api_key:'key-f04b0e48c5a6fc439036aa284b5e15e6'
 };
 
 module.exports = config;
--- main/routes/mailgun.js
+++ main/routes/mailgun.js
@@ -1,23 +1,23 @@
 var Mailgun = require('mailgun-js');
-var api_key = 'key-f04b0e48c5a6fc439036aa284b5e15e6';
-var domain = 'mailgun.treatsfromuk.com';
-var from_who = 'nirav@treatsfromuk.com';
 
-module.exports = function (app) {
+module.exports = function (app,config) {
 
     app.post('/mailgun/new', function (req, res) {
 
-        console.log(req.body);
+        var api_key = config.mg_api_key;
+
+        var domain = 'mailgun.treatsfromuk.com';
+        var from_who = 'nirav@treatsfromuk.com';
+
         var mailgun = new Mailgun({apiKey: api_key, domain: domain});
 
         var data = {
             from: from_who,
             to: "nirav.shah83@gmail.com",
             subject: 'New User Query',
-            text: "Test Email"
+            html: "<p>Name: " + req.body.user_name + "</p><p> Email: " + req.body.user_email + "</p><p> Message: " + req.body.user_message + "</p>"
         };
 
-        //Invokes the method to send emails given the above data with the helper library
         mailgun.messages().send(data, function (err, body) {
             console.log('Error: ',err);
             console.log(body);
